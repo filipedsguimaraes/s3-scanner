7e10c837e3ee9965ab2cd68f9655c267e1052ccb
filipedsguimaraes <filipedsguimaraes@outlook.com>
2025-07-10 08:24:43 +0100

    Logo, requirements and patchs

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 from botocore.config import Config
 from botocore import UNSIGNED
+import botocore
 import argparse
 import boto3
 import os
 
 parser = argparse.ArgumentParser(
                     prog = 'main.py',
-                    description = 'D',
+                    description = 'A tool to list and download files from an s3',
                     epilog = 'Github: filipedsguimaraes')
-parser.add_argument('-u', '--url', required=True, type=str, help="main.py -u s3://bucketname/")
-parser.add_argument('-o', '--output', required=False, action='store_true', help="Use -o to set output true")
+parser.add_argument('-u', '--url', required=True, type=str, help="main.py -u flaws.cloud")
+parser.add_argument('-o', '--output', required=False, action='store_true', help="Use -o to download all files")
 args = parser.parse_args()
+
+def banner():
+    os.system("cls")
+    return r"""
+ ____ _____   ____                                  
+/ ___|___ /  / ___|  ___ __ _ _ __  _ __   ___ _ __ 
+\___ \ |_ \  \___ \ / __/ _` | '_ \| '_ \ / _ \ '__|
+ ___) |__) |  ___) | (_| (_| | | | | | | |  __/ |   
+|____/____/  |____/ \___\__,_|_| |_|_| |_|\___|_|   
+    """
 
 class aws_commands:
     def __init__(self, url):
-        self.bucket_name = url
+        print(banner())
+        self.bucket_name = url.replace("s3://", "").replace("/", "")
         self.s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
-
+            
     # List all objects from an bucket
-    def list_objects(self, bucket_name, dir=""):
-
+    def list_objects(self, dir=""):
         # Unsigned Connection
-        response = self.s3.list_objects_v2(Bucket=bucket_name, Prefix=dir)
+        response = self.s3.list_objects_v2(Bucket=self.bucket_name, Prefix=dir)
 
         if not dir:
-            print(f"Listing objects from: {bucket_name}\n")
+            print(f"Listing objects from: {self.bucket_name}\n")
 
         if 'Contents' in response:
             for obj in response['Contents']:
@@ -41,8 +52,9 @@ class aws_commands:
 
                 # Enter in a directory
                 if next_dir:
-                    self.list_objects(bucket_name, dir+next_dir)
+                    self.list_objects(dir+next_dir)
 
+    # Download all s3 files
     def download(self, file, dir=""):
         os.makedirs(f"results/{self.bucket_name}", exist_ok=True)
         data = file.rsplit('/', 1)
@@ -53,13 +65,16 @@ class aws_commands:
 
         if "" not in data: 
             self.s3.download_file(self.bucket_name, file, f"results/{self.bucket_name}/{file}")
-    
+
+
 def main():
-    os.system("cls")
     aws = aws_commands(args.url)
-    aws.list_objects(args.url)
+    aws.list_objects()
 
 try:
     main()
+except botocore.exceptions.ClientError as error:
+    _, msg, bucket_name = error.response["Error"].values()
+    print(f"Bucket Name: {bucket_name}\nError: {msg}")
 except KeyboardInterrupt:
     exit()
